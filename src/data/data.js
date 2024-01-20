33398641cc76469bbdc0342bfecfd71a116eaeb9
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-20 15:16:52 +0530

    feat: data

---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+export const variantsData = [
+  "30 min",
+  "60 min",
+  "90 min",
+  "120 min",
+  "150 min",
+  "180 min",
+];
+
+export const slotsData = [
+  "03:30 AM - 04:00 AM",
+  "04:00 AM - 04:30 AM",
+  "04:30 AM - 05:00 AM",
+  "05:00 AM - 05:30 AM",
+];
