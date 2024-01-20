9e4519885b9d07e017fdb32f75a0950be6fc6b1b
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-20 17:47:29 +0530

    feat: calendar component

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,16 +1,32 @@
 import * as React from "react";
+import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { useData } from "../../context/DataProvider";
 
 const Calendar = () => {
-  const handleChange = () => {};
+  const { state, dispatch } = useData();
+
+  const handleChange = (e) => {
+    const originalDate = new Date(e.$d);
+    const formattedDate = originalDate.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+  
+    dispatch({ type: "DATE_SELECTED", payload: formattedDate });
+  };
   return (
-   <div className="border bg-[#f2f2f5] border-[#C7C9D9] rounded-[10px]">
-     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar onChange={handleChange} />
-    </LocalizationProvider>
-   </div>
+    <div className="border bg-[#f2f2f5] border-[#C7C9D9] rounded-[10px]">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar
+          onChange={handleChange}
+          defaultValue={dayjs("2024-01-20")}
+        />
+      </LocalizationProvider>
+    </div>
   );
 };
 
