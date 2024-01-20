623348b462a14a3ac310c45b2d05865fe764d4d9
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-20 15:16:39 +0530

    feat: box

---
+++ src/components/box/Box.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Box = ({ children, style }) => {
+  return (
+    <div
+      className={`flex items-center justify-center border border-[#C7C9D9] h-[48px] leading-[21px] rounded-[10px] px-[12px] py-[8px] text-[14px] ${style}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Box;
