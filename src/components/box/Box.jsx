76ca4dfc5d3a986a6395ba0bc5da23eb0f4dc561
prachi-sahu-codes <prachisahu998@gmail.com>
2024-01-20 17:47:43 +0530

    feat: box

--- src/components/box/Box.jsx
+++ src/components/box/Box.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Box = ({ children, style }) => {
   return (
     <div
-      className={`flex items-center justify-center border border-[#C7C9D9] h-[48px] leading-[21px] rounded-[10px] px-[12px] py-[8px] text-[14px] ${style}`}
+      className={`flex border border-[#C7C9D9] h-[48px] leading-[21px] rounded-[10px] px-[12px] py-[8px] text-[14px] ${style}`}
     >
       {children}
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Navbar from "./navbar/Navbar";
 import Box from "./box/Box";
 import Calendar from "./calendar/Calendar";
 import Container from "./container/Container";
+import VariantSelect from "./variantSelect/VariantSelect";
+import SlotsSelect from "./slotsSelect/SlotsSelect";
 
-export { Navbar, Box, Calendar, Container };
+export { Navbar, Box, Calendar, Container, VariantSelect, SlotsSelect };
